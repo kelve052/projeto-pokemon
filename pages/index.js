f9380bbf0a6e47ce15e5a29f92611123cd7dc1ee
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-03-01 15:20:41 -0300

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
